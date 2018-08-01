7537b1f37b7dbce295e298a1e129028d5144cbad
lliyuu520 <lliyuu520@gmail.com>
2018-08-01 11:18:55 +0800

    修改脚本8

--- eureka-server.sh
+++ eureka-server.sh
@@ -25,7 +25,7 @@ start(){
   if [ $? -eq "0" ]; then
     echo "${APP_NAME} is already running. pid=${pid} ."
   else
-    nohup java -Xms512m -Xmx624m -jar eureka/eureka-server/target/eureka-server.jar &
+    nohup java -Xms512m -Xmx624m -jar $APP_NAME &
   fi
 }
 
